a29c42049cdd473cd6d2c4c30e2656d58b87fa63
KoynovStas <skojnov@yandex.ru>
2017-06-11 10:42:57 +0400

    add more tests in example many_tests

--- examples/many_tests.c
+++ examples/many_tests.c
@@ -3,32 +3,58 @@
 
 
 
-TEST(test_1)
+TEST(test_pass)
 {
     TEST_PASS();
 }
 
 
 
-TEST(test_2)
+TEST(test_pass2)
+{
+    TEST_PASS("a message for pass");
+}
+
+
+
+TEST(test_skip)
 {
     TEST_SKIP();
 }
 
 
 
-TEST(test_3)
+TEST(test_skip2)
+{
+    TEST_SKIP("a message for skip");
+}
+
+
+
+TEST(test_fail)
 {
     TEST_FAIL();
 }
 
 
 
+TEST(test_fail2)
+{
+    TEST_FAIL("a message for fail");
+}
+
+
+
 ptest_func tests[] =
 {
-    test_1,
-    test_2,
-    test_3,
+    test_pass,
+    test_pass2,
+
+    test_skip,
+    test_skip2,
+
+    test_fail,
+    test_fail2,
 };
 
 
