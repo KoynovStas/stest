28fb6451266f93a32fa88f7daaab34e9e0e96aab
KoynovStas <skojnov@yandex.ru>
2017-06-11 10:05:04 +0400

    add macro TEST_SKIP(msg)

--- stest.h
+++ stest.h
@@ -73,6 +73,7 @@ struct test_info_t
 
 
 #define TEST_PASS(msg)  return (struct test_info_t) {__FILE__, __func__, __LINE__, STATUS_PASS, msg}
+#define TEST_SKIP(msg)  return (struct test_info_t) {__FILE__, __func__, __LINE__, STATUS_SKIP, msg}
 
 
 
